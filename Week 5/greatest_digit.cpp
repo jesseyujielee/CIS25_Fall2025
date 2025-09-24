371024bf5ecc430ddeff422d810a0d9ee29972be
Jesse Lee <jesseyujielee@gmail.com>
2025-09-23 22:48:08 -0700

    Create greatest_digit.cpp

---
+++ Week 5/greatest_digit.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <cmath>
+
+int greatest_digit(int num) {
+
+	int greatest{0};
+	std::string num_str = std::to_string(num);
+	for (int i = 0; i < num_str.length(); i++) {
+		int digit{ num % static_cast<int>(std::pow(10, i)) };
+		if (digit > greatest)
+			greatest = digit;
+	}
+}
