c716f9f503f3903b65d523cf5d437d2486cbe814
Jesse Lee <jesseyujielee@gmail.com>
2025-09-23 23:33:34 -0700

    Finish menu and add 3rd choice

--- Week 5/greatest_digit.cpp
+++ Week 5/greatest_digit.cpp
@@ -5,10 +5,13 @@
 int greatest_digit(int num) {
 
 	int greatest{0};
-	std::string num_str = std::to_string(num);
-	for (int i = 0; i < num_str.length(); i++) {
-		int digit{ num % static_cast<int>(std::pow(10, i)) };
-		if (digit > greatest)
+	while (num != 0) {
+		int digit{ num % 10 };
+		if (digit > greatest) {
 			greatest = digit;
+		}
+		num /= 10;
 	}
+
+	return greatest;
 }
--- Week 5/week_5_menu.cpp
+++ Week 5/week_5_menu.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
 #include "greatest_digit.h"
 
+double exponentiate(double base, int exp) {
+
+	double result{1};
+	
+	for (int i{ 0 }; i < std::abs(exp); i++) {
+		result *= base;
+	}
+
+	if (exp < 0) {
+		return 1.0 / result;
+	}
+
+	return result;
+}
+
 int main() {
+
+	int choice{ 0 };
+	do {
+		std::cout << "Menu:\n1 - exit\n2 - greatest digit\n3 - exponentiate\n";
+		std::cin >> choice;
+		if (choice == 2) {
+			std::cout << "Enter an int: ";
+			int input{};
+			std::cin >> input;
+			std::cout << greatest_digit(input) << "\n";
+		}
+		else if (choice == 3) {
+			std::cout << "Enter a base and an exponent (int): ";
+			double base{};
+			int exp{};
+			std::cin >> base >> exp;
+			std::cout << exponentiate(base, exp) << "\n";
+		}
+	} while (choice != 1);
 
 	return 0;
 }
